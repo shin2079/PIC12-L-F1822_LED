adb7e413677a7f2c8dcb059d755b146dcbf38d70
Shinichiro Watanabe <shinw.web@gmail.com>
2017-01-21 20:42:47 +0900

    Update PIC12LF1822_LED.c

--- PIC12LF1822_LED.c
+++ PIC12LF1822_LED.c
@@ -1,7 +1,3 @@
-// PIC12F1822 Configuration Bit Settings
-
-// 'C' source line config statements
-
 // CONFIG1
 #pragma config FOSC = INTOSC
 #pragma config WDTE = OFF
@@ -27,19 +23,16 @@
 #include <xc.h>
 
 void main(void) {
-//pic init
-OSCCON = 0b01111010; //クロック設定 16MHz
-ANSELA = 0x00; //すべてのピンをデジタルモードに設定
-TRISA = 0b00001000; //I/O設定　0出力 1入力　RA3は入力専用なので1
+    //pic init
+    OSCCON = 0b01111010; //クロック設定 16MHz
+    ANSELA = 0x00;       //すべてのピンをデジタルモードに設定
+    TRISA = 0b00001000;  //I/O設定　0出力 1入力　RA3は入力専用なので1
 
-while(1) {
-    LATAbits.LATA4 = 0; //LED点灯 RA4をLow
-    //PORTA = 0b00010000;
-    __delay_ms(100);    //100ms待つ
-    
-    LATAbits.LATA4 = 1; //LED消灯 RA4をHi
-   //PORTA = 0b00000000;
-    __delay_ms(100);    //100ms待つ
-}
+    while(1) {
+        LATAbits.LATA4 = 0; //LED点灯 RA4をLow
+        __delay_ms(100);    //100ms待つ
+        LATAbits.LATA4 = 1; //LED消灯 RA4をHi
+        __delay_ms(100);    //100ms待つ
+    }
 return;
 }
